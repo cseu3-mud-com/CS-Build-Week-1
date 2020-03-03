c97072b028cb608f811d79b7a0201f4c20248df0
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-03 09:00:00 +0000

    Added /rooms endpoint and /genMap

--- adventure/api.py
+++ adventure/api.py
@@ -28,7 +28,6 @@ def initialize(request):
     players = room.playerNames(player_id)
     return JsonResponse({'uuid': uuid, 'name':player.user.username, 'title':room.title, 'description':room.description, 'players':players}, safe=True)
 
-
 @csrf_exempt
 @api_view(["GET"])
 def generateWorld(request):
@@ -154,6 +153,11 @@ def generateWorld(request):
                 worldMap[row][column] = RoomSerializer(room).data
 
     return JsonResponse({ "worldMap": worldMap })
+
+@csrf_exempt
+@api_view(["GET"])
+def getRooms(request):
+    return JsonResponse({ "rooms":[RoomSerializer(room).data for room in Room.objects.all()]})
 
 # @csrf_exempt
 @api_view(["POST"])
--- adventure/urls.py
+++ adventure/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import url
 from . import api
 
 urlpatterns = [
+    url('genMap', api.generateWorld),
     url('init', api.initialize),
     url('move', api.move),
     url('say', api.say),
-    url('rooms', api.generateWorld),
+    url('rooms', api.getRooms),
 ]
