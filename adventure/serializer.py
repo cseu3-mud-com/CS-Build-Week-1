c99bfea11c98a90503946d956a2ecc0c30526d2c
Melquisedeque Pereira <melqui1@live.com.pt>
2020-03-03 20:03:43 +0000

    Added x and y to room serializer

--- adventure/serializer.py
+++ adventure/serializer.py
@@ -4,4 +4,4 @@ from .models import Room
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
-        fields = ['id', 'title', 'description', 'n_to', 's_to', 'e_to', 'w_to']
+        fields = ['id', 'n_to', 's_to', 'e_to', 'w_to', 'x', 'y', 'title', 'description']
